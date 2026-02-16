f919795cdb9f209922baa1b78d312647ae55931e
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-02-16 18:37:38 +0530

    react practice2

--- app.js
+++
@@ -1,3 +0,0 @@
-const box = ()=> React.createElement('div',{id:'box'},'this is from box')
-
-export default box;
---
+++ class0/src/App.jsx
@@ -0,0 +1,18 @@
+// rafce
+//npm run dev to run
+// cd class0
+// npm run dev
+
+
+
+
+const App = () => {
+  return (
+    <div>
+    <div>App</div>
+    <button>Click</button>
+    </div>
+  )
+}
+
+export default App
