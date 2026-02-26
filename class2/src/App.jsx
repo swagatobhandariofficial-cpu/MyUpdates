e9b721c851e09cdb5f53976398041c2bfc7183b2
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-02-26 17:59:26 +0530

    Button on click function

--- class2/src/App.jsx
+++ class2/src/App.jsx
@@ -1,13 +1,17 @@
-
-import Men from './components/Men'
-import Navbar from './components/Navbar'
-import Women from './components/Women'
+import React from 'react'
 
 const App = () => {
-  const age=12;
+  function btnClick(val) {
+    console.log('Puuchhuk', val);
+  }
   return (
-    <div className='p-4' >
-      {age>=18 ? <Men/> : <Women/>}
+    <div className='p-6'>
+      <button
+        // onClick={btnClick} //or
+        onClick={function () {
+          btnClick("done")
+        }}
+        className='active:scale-95 cursor-pointer px-3 py-2.5 bg-blue-600 rounded-xl text-2xl font-semibold'>Click to Download</button>
     </div>
   )
 }
