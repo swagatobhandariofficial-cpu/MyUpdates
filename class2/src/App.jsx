038609248c42fd35fd46a0218a3c242a3ee08996
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-02-28 02:48:09 +0530

    gender

--- class2/src/App.jsx
+++ class2/src/App.jsx
@@ -1,38 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const name = ['Swagato', 'Disha', 'Debanjan', 'Srija', 'Subham', 'Badam']
-  const [marks, setMarks] = useState([32, 34, 66, 90, 80, 99])
-  
-
-  function grace() {
-    const newMarks = marks.map(function (elm) {
-      if(elm>=95){
-        return elm
-      }
-      else{
-        return elm + 5
-      }
-    })
-    setMarks(newMarks)
-  }
-
   return (
     <div>
-      {marks.map(function (elem, idx) {
-        return  <h1 key={idx}>
-                  {name[idx]} of student {idx + 1} is: {elem} ({elem>=35?'PASS':'FAIL'}) — Grade: {
-                        elem >= 90 ? 'A+' :
-                        elem >= 75 ? 'A' :
-                        elem >= 60 ? 'B' :
-                        elem >= 50 ? 'C' :
-                        elem >= 35 ? 'D' :
-                        'F'
-  }
-                </h1>
-
-      })}
-      <button onClick={grace}> Give them Grace</button>
+      <div>
+        <input type="radio" name="gender" id="male" />
+        <label htmlFor="male">Male</label>
+      </div>
+      <div>
+        <input type="radio" name="gender" id="female" />
+        <label htmlFor="female">Female</label>
+      </div>
     </div>
   )
 }
