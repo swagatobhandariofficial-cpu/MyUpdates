eb13e4f39782fe34bca2824c81f2bbc9ebb9441c
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-02-28 01:22:28 +0530

    Use state 6

--- class2/src/App.jsx
+++ class2/src/App.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  const marks =[20,23,66,90,80,99]
-  const name  =['Swagato','Disha','Debanjan','Srija','Subham','Badam']
+  const name = ['Swagato', 'Disha', 'Debanjan', 'Srija', 'Subham', 'Badam']
+  const [marks, setMarks] = useState([32, 34, 66, 90, 80, 99])
+  
+
+  function grace() {
+    const newMarks = marks.map((elm)=> elm+5)
+    setMarks(newMarks)
+  }
+
   return (
     <div>
-      {marks.map(function(elem,idx){
-        return <h1>{name[idx]} of student {idx+1} is: {elem+100}</h1>
-        
+      {marks.map(function (elem, idx) {
+        return  <h1 key={idx}>
+                  {name[idx]} of student {idx + 1} is: {elem}
+                </h1>
+
       })}
+      <button onClick={grace}> Give them Grace</button>
     </div>
   )
 }
