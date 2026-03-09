2411335d64370c3bfed1baf1bfe26eb13a922ff7
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-03-09 17:41:52 +0530

    useEffect Demo

--- class4/src/App.jsx
+++ class4/src/App.jsx
@@ -1,29 +1,36 @@
-import React, { useState } from 'react'
-import axios, { all } from "axios";
-import User from './User';
-
-
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
 
-  const [allData, setAllData] = useState([])
 
-  async function getData() {
-    const promis = await axios.get('https://jsonplaceholder.typicode.com/users')
-    console.log(promis.data.name);
-    setAllData(promis.data)
-  }
+  const [firstNumber, setFirstNumber] = useState('00')
+  const [secondNumber, setSecondNumber] = useState('00')
+  const [thirdNumber, setThirdNumber] = useState('00')
+
+  useEffect(() => {
+    console.log("use effect running");
+
+  }, [secondNumber])
   return (
-    <div className='p-10 '>
-      <button className='px-4 py-3 bg-emerald-400 hover:bg-emerald-300 poi rounded-2xl active:scale-95 cursor-pointer' onClick={getData}>Click Me🍒</button>
 
-      <div className='flex flex-wrap justify-evenly'>
-        {allData.map(function (elm, idx) {
-          return <div key={idx}>
-            <User elm={elm}/>
-          </div>
-        })}
-      </div>
+
+    <div className='p-5 h-screen bg-black text-white'>
+      <h1 className='p-0.5 text-5xl text-amber-200 m-0.5'>{firstNumber}</h1>
+      <button className='px-2.5 py-3 bg-emerald-500 rounded-2xl text-xl active:scale-95' onClick={() => {
+        setFirstNumber(Math.floor(Math.random() * 100))
+      }}>Change Number1</button>
+      <br />
+      <br />
+      <h1 className='p-0.5 text-5xl text-blue-300 m-0.5'>{secondNumber}</h1>
+      <button className='px-2.5 py-3 bg-emerald-500 rounded-2xl text-xl active:scale-95' onClick={() => {
+        setSecondNumber(Math.floor(Math.random() * 100))
+      }}>Change Number2</button>
+      <br />
+      <br />
+      <h1 className='p-0.5 text-5xl text-red-400 m-0.5'>{thirdNumber}</h1>
+      <button className='px-2.5 py-3 bg-emerald-500 rounded-2xl text-xl active:scale-95' onClick={() => {
+        setThirdNumber(Math.floor(Math.random() * 100))
+      }}>Change Number3</button>
     </div>
   )
 }
