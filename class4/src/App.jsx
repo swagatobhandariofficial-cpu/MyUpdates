ed819a812e1a8ce17fbd2005e22af60c6e123f5f
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-03-09 18:04:43 +0530

    api fetching in use Effect

--- class4/src/App.jsx
+++ class4/src/App.jsx
@@ -1,36 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import axios from "axios";
 
 const App = () => {
+  const getData = async function () {
+    const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon')
+    console.log(pokemon);
+  }
 
+  useEffect(function () {
+    getData()
+  })
 
-  const [firstNumber, setFirstNumber] = useState('00')
-  const [secondNumber, setSecondNumber] = useState('00')
-  const [thirdNumber, setThirdNumber] = useState('00')
-
-  useEffect(() => {
-    console.log("use effect running");
-
-  }, [secondNumber])
   return (
-
-
-    <div className='p-5 h-screen bg-black text-white'>
-      <h1 className='p-0.5 text-5xl text-amber-200 m-0.5'>{firstNumber}</h1>
-      <button className='px-2.5 py-3 bg-emerald-500 rounded-2xl text-xl active:scale-95' onClick={() => {
-        setFirstNumber(Math.floor(Math.random() * 100))
-      }}>Change Number1</button>
-      <br />
-      <br />
-      <h1 className='p-0.5 text-5xl text-blue-300 m-0.5'>{secondNumber}</h1>
-      <button className='px-2.5 py-3 bg-emerald-500 rounded-2xl text-xl active:scale-95' onClick={() => {
-        setSecondNumber(Math.floor(Math.random() * 100))
-      }}>Change Number2</button>
-      <br />
-      <br />
-      <h1 className='p-0.5 text-5xl text-red-400 m-0.5'>{thirdNumber}</h1>
-      <button className='px-2.5 py-3 bg-emerald-500 rounded-2xl text-xl active:scale-95' onClick={() => {
-        setThirdNumber(Math.floor(Math.random() * 100))
-      }}>Change Number3</button>
+    <div>
+      hiioi
     </div>
   )
 }
