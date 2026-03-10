71654ba735d556c151028cb0bcbfeb8c59de235e
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-03-10 12:19:21 +0530

    pokemon api

--- class4/src/App.jsx
+++ class4/src/App.jsx
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios";
 
 const App = () => {
+  const [pokemon, setPokemon] = useState([])
   const getData = async function () {
-    const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon')
-    console.log(pokemon);
+    const pkmn = await axios.get('https://pokeapi.co/api/v2/pokemon')
+    setPokemon(pkmn.data.results);
   }
-
-  useEffect(function () {
+  useEffect(function(){
     getData()
-  })
+  },[])
 
   return (
-    <div>
-      hiioi
+    <div className='p-5'>
+      <button className='px-3.5 py-3 active:scale-95 bg-emerald-500 rounded-xl' onClick={getData}>Show Pokemon</button>
+
+      {pokemon.map(function(elm,idx){
+        return <h1 key={idx}>{elm.name}</h1>
+      })}
     </div>
   )
 }
