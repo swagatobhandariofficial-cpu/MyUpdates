1303d15539ad022fb69151a101195f28a265b44d
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-16 01:14:15 +0530

    Methods of set alg

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-s = { 1, 2, 3, 4 , 5,5,5,4,4}
-s.add(6)
-print(s)
-s.remove(5)
-print(s)
-s.discard(4)
-print(s)
-popped_elm = s.pop()
-print(popped_elm)
+s1 = {1,2,3,4,5}
+s2 = {3,5,6,7,8}
+
+uni = s1.union(s2) # uni = s1|s2
+print(uni)
+
+inter = s1.intersection(s2) # inter = s1 & s2
+print(inter)
+
+sym_diffe = s1.symmetric_difference(s2) # sym_diffe = s1 ^ s2
+print(sym_diffe)
+
+diff = s1.difference(s2) # diff = s1 - s2
+print(diff)
+
+s1 -=s2
+print(s1)
