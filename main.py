e9b8099a3621503b944a01e63c9733134a4c7318
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-16 00:33:21 +0530

    Methods in set

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-a,b,c,d,e = (1,22,333,4444,55555)
-
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
+s = { 1, 2, 3, 4 , 5,5,5,4,4}
+s.add(6)
+print(s)
+s.remove(5)
+print(s)
+s.discard(4)
+print(s)
+popped_elm = s.pop()
+print(popped_elm)
