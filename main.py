7d30394155f7c1120d4d3906d94e25f647c5d5a4
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-15 19:47:45 +0530

    Checking if list is sorted or not

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-a = [22,34,2.4,99,-45,-33,-7,39]
-largst = a[0]
-secLargst = a[0]
-for i in range(len(a)):
-    if a[i] > largst :
-        secLargst = largst
-        largst = a[i]
-    elif a[i]> secLargst:
-        secLargst = a[i]
-
-
-print(f"largest={largst} , Second Largest is = {secLargst} ")
+# a = [22,34,2.4,99,-45,-33,-7,39]
+a = [1,2,3,5,7,9,55,67]
+for i in range(len(a)-1):
+    if a[i]<a[i+1] :
+        continue
+    else :
+        print("Your List is not sorted.")
+        break
+else :
+    print("Your List is sorted")
