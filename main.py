01b3497cc3bb1a27f855b777e2ed81c0bd869025
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-16 21:38:28 +0530

    Merging two dictionary

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-di = {10:100 , 20:200, 30:300} #crud operation
+d1 = {10:100 , 20:200, 30:300} #crud operation
+d2 = {40:400, 50:500, 60:600}
 
-for i in di:
-    print(di[i])
+for i in d2:
+    d1[i]=d2[i]
 
-di2 = di.copy()
-print(di2)
+print[d1]
