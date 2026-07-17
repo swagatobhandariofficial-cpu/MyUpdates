318e78acde9253bbe85d032fd884e20a6f0a8b68
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-17 23:07:00 +0530

    Exception Handling

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-a = [1,1,2,1,4,3,2,1,4,5,6,7,9,8,8]
+a = int(input("Telll your num:"))
 
-d={}
+try:
+    print(10/a)
+    print("Calculation done!!!")
+except ZeroDivisionError:
+    print("Try again.")
 
-for i in a:
-    if i in d.keys():
-        d[i]+=1
-    else:
-        d[i]=1
-print(d)
+
+
