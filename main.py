3e65f2ccea10c02245dd09079fcf545d9f3cbb70
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-17 23:21:29 +0530

    Exception Handling 3

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ a = int(input("Telll your num:"))
 try:
     print(10/a)
     print("Calculation done!!!")
-except ZeroDivisionError:
-    print("Try again.")
-
-
-
+except Exception as err:
+    print(f"Try again. Your Error is: '{err}'")
+else:
+    print("THere is no exception")
+finally:
+    print("It will run even if any exception occurs.")
