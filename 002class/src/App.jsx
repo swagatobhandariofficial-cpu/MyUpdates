c714b32cc8d48b970abf25eedf63a6b9489e8bb9
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-23 22:16:28 +0530

    Increase Decrease usestate react

--- 002class/src/App.jsx
+++ 002class/src/App.jsx
@@ -8,12 +8,17 @@ const App = () => {
     <div>
       <h1>{num}</h1>
       <div>
-        <button onClick={()=>{
-        setnNum(num+1)
-      }}>Increase</button>
-      <button onClick={()=>{
-        setnNum(num-1)
-      }}>Decrease</button>
+        <button onClick={() => {
+          setnNum(num + 1)
+        }}>Increase</button>
+
+        <button onClick={() => {
+          setnNum(num - 1)
+        }}>Decrease</button>
+
+        <button onClick={() => {
+          setnNum(num + 10)
+        }}>Jump by 10</button>
       </div>
     </div>
   )
