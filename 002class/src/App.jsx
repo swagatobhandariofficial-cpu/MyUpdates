d011e4298812304e438e029644e9ea8b0039e9a2
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-24 01:43:35 +0530

    radio

--- 002class/src/App.jsx
+++ 002class/src/App.jsx
@@ -1,28 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-
-  const [marks, setMarks] = useState([88, 78, 96, 35, 34])
-
-  function grace() {
-    const newMa = marks.map(function (elm) {
-      if (elm<95) {
-        return elm+5
-      }
-      else{
-        return elm
-      }
-    })
-    setMarks(newMa)
-  }
-
   return (
-    <div>
-      {marks.map(function (elm, idx) {
-        return <h1 key={idx}> Marks of students {idx + 1} is {elm}  ({elm>35? 'PASS':'FAIL'}) </h1>
-      })}
+    <div className='parent'>
+      <div>
+        <input type="radio" name="gender" id="male" />
+        <label htmlFor="male"> Male</label>
+      </div>
 
-      <button onClick={grace}>Give them grace of 5 marks</button>
+      <div>
+        <input type="radio" name="gender" id="female" />
+        <label htmlFor="female"> Female</label>
+      </div>
     </div>
   )
 }
