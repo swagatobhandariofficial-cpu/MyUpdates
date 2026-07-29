78b82252ff00f14a3c436cdb8d32c2b93f6afa96
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-07-29 12:34:03 +0530

    Custom profile creation using react

--- 004class/src/App.jsx
+++ 004class/src/App.jsx
@@ -1,15 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from './components/Card'
 
 const App = () => {
+
+  const [name, setName] = useState('')
+  const [url, setUrl] = useState('')
+  const [role, setRole] = useState('')
+  const [description, setDescription] = useState('')
+
+  const [allUsers, setAllUsers] = useState([])
+
+
+  const submthndl = (e)=>{
+    e.preventDefault()
+    const oldUsers = [...allUsers]
+    oldUsers.push({name, url, role, description})
+    setAllUsers(oldUsers)
+    console.log(oldUsers);
+    
+
+
+    console.log(name, url, role, description);
+
+    setName('')
+    setRole('')
+    setUrl('')
+    setDescription('')
+  }
+
   return (
     <div className='h-screen bg-black text-white'>
-      <form >
-        <input className='m-3 px-5 py-2.5 border-1.5 rounded-xl' type="text" placeholder='Enter your name' />
-        <input className='m-3 px-5 py-2.5 border-1.5 rounded-xl' type="text" placeholder='image url' />
-        <input className='m-3 px-5 py-2.5 border-1.5 rounded-xl' type="text" placeholder='Enter Role' />
-        <input className='m-3 px-5 py-2.5 border-1.5 rounded-xl' type="text" placeholder='Enter Description' />
-        <button className='bg-white transition-all active:bg-blue-800 active:text-white active:scale-95 text-black px-2 py-1.5 rounded-xl'>Submit</button>
+      <form onSubmit={(e)=>{
+        submthndl(e)
+      }}>
+        <input value={name} onChange={(e)=>{
+          setName(e.target.value)
+          
+        }} className='m-3 px-5 py-2.5 border-1.5 rounded-xl' type="text" placeholder='Enter your name' />
+
+        <input value={url} onChange={(e)=>{
+          setUrl(e.target.value)
+          
+        }} className='m-3 px-5 py-2.5 border-1.5 rounded-xl' type="text" placeholder='image url' />
+
+        <input value={role} onChange={(e)=>{
+          setRole(e.target.value)
+          
+        }} className='m-3 px-5 py-2.5 border-1.5 rounded-xl' type="text" placeholder='Enter Role' />
+
+        <input value={description} onChange={(e)=>{
+          setDescription(e.target.value)
+          
+        }} className='m-3 px-5 py-2.5 border-1.5 rounded-xl' type="text" placeholder='Enter Description' />
+
+        <button className='bg-white m-2 transition-all active:bg-blue-800 active:text-white active:scale-95 cursor-pointer text-black px-2 py-1.5 rounded-xl '>Submit</button>
       </form>
+
+      <div className='m-4 px-5 py-4 border-1.5 rounded-xl flex flex-wrap gap-3' >
+        <Card/>
+        <Card/>
+        {/* {allUsers.map(()=>{
+          return <Card/>
+        })} */}
+      </div>
     </div>
   )
 }
---
+++ 004class/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-[20vw] h-80 rounded-xl text-center p-5 flex items-center flex-col bg-white text-taupe-950 '>
+      <img className='h-22 w-22 rounded-full' src="https://images.unsplash.com/photo-1660730531500-30f2ef704442?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1 className='text-xl font-semibold m-0.5'>Swagato</h1>
+        <h6 className='text-sm text-blue-400 font-semibold my-0.5'>Developer</h6>
+        <p className='py-2.5 px-4 text-sm my-0.5 font-light leading-tight '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ratione, at esse velit atque iste praesentium minima a floral.</p>
+        <button className='text-white my-2.5 px-3 py-1.5 rounded-[8px] text-sm bg-red-500 active:bg-red-400 active:scale-95 cursor-pointer transition-colors '>Remove</button>
+    </div>
+  )
+}
+
+export default Card
